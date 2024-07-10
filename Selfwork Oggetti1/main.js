9ee64cda802c5176af59076f82aff0f29d37b02f
Angelo Sanfilippo <124434790+AngeloSan86@users.noreply.github.com>
2024-07-10 16:17:27 +0200

    Add files via upload

---
+++ Selfwork Oggetti1/main.js
@@ -0,0 +1,10 @@
+let persona = {
+    "nome" : "Angelo",
+    "cognome" : "Sanfilippo",
+    "eta'" : 38,
+    saluta : function() {
+        console.log(`Ciao a tutti mi chiamo ${persona.nome} ${persona.cognome}`)
+    }
+};
+
+persona.saluta();
